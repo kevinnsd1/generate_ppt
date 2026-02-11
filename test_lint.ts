59414b824ffd736769afeac67572245351714d97
Kevin Satria D <130447401+kevinnsd1@users.noreply.github.com>
2026-02-11 13:47:16 +0700

    chore: delete test lint file.

--- test_lint.ts
+++
@@ -1,7 +0,0 @@
-// This file contains intentional lint errors to test Husky
-// prefer-const violation: 'x' is never reassigned, should be 'const'
-let x = 10;
-console.log(x);
-
-// no-unused-vars violation (if enabled): 'y' is defined but never used
-const y = 20; 
